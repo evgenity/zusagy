b8fcdabd038391e73273e55d914d40f6f7e254f7
Evgeni <bikovevg@gmail.com>
2022-07-29 13:29:45 +0300

    basic functions added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'promises.apps.PromisesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/promises/models.py
+++ backend/promises/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Promise(models.Model):
+    pub_date = models.DateTimeField('date published')
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.email
