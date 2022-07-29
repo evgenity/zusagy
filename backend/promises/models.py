3147560d19e789ab4d6ba8696edd03709dce5d46
Evgeni <bikovevg@gmail.com>
2022-07-29 13:33:09 +0300

    added shortlink field to model

--- backend/promises/models.py
+++ backend/promises/models.py
@@ -5,6 +5,7 @@ class Promise(models.Model):
     pub_date = models.DateTimeField('date published')
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
+    short_link = models.CharField(max_length=200)
 
     def __str__(self):
         return self.email
